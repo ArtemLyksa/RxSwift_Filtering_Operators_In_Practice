ed5116fb0dc7760c0c9c85877057151ad00ffbc9
Artem Lyksa <artemlyksa@Artems-MacBook-Pro-3.local>
2017-09-11 11:13:33 +0300

    starter finished

--- Combinestagram/MainViewController.swift
+++ Combinestagram/MainViewController.swift
@@ -24,83 +24,120 @@ import UIKit
 import RxSwift
 
 class MainViewController: UIViewController {
-
-  @IBOutlet weak var imagePreview: UIImageView!
-  @IBOutlet weak var buttonClear: UIButton!
-  @IBOutlet weak var buttonSave: UIButton!
-  @IBOutlet weak var itemAdd: UIBarButtonItem!
-
-  private let bag = DisposeBag()
-  private let images = Variable<[UIImage]>([])
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    images.asObservable()
-      .subscribe(onNext: { [weak self] photos in
-        guard let preview = self?.imagePreview else { return }
-        preview.image = UIImage.collage(images: photos,
-                                        size: preview.frame.size)
-      })
-      .disposed(by: bag)
-
-    images.asObservable()
-      .subscribe(onNext: { [weak self] photos in
-        self?.updateUI(photos: photos)
-      })
-      .disposed(by: bag)
-
-  }
-
-  private func updateUI(photos: [UIImage]) {
-    buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
-    buttonClear.isEnabled = photos.count > 0
-    itemAdd.isEnabled = photos.count < 6
-    title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
-  }
-
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    print("resources: \(RxSwift.Resources.total)")
-  }
-
-  @IBAction func actionClear() {
-    images.value = []
-  }
-
-  @IBAction func actionSave() {
-    guard let image = imagePreview.image else { return }
-
-    PhotoWriter.save(image)
-      .subscribe(onError: { [weak self] error in
-        self?.showMessage("Error", description: error.localizedDescription)
-      }, onCompleted: { [weak self] in
-        self?.showMessage("Saved")
-        self?.actionClear()
-      })
-      .disposed(by: bag)
-  }
-
-  @IBAction func actionAdd() {
-    //images.value.append(UIImage(named: "IMG_1907.jpg")!)
-    let photosViewController = storyboard!.instantiateViewController(
-      withIdentifier: "PhotosViewController") as! PhotosViewController
-
-    photosViewController.selectedPhotos
-      .subscribe(onNext: { [weak self] newImage in
-        guard let images = self?.images else { return }
-        images.value.append(newImage)
-      }, onDisposed: {
-        print("completed photo selection")
-      })
-      .disposed(by: photosViewController.bag)
-
-    navigationController!.pushViewController(photosViewController, animated: true)
-  }
-
-  func showMessage(_ title: String, description: String? = nil) {
-    alert(title: title, text: description)
-      .subscribe()
-      .disposed(by: bag)
-  }
+    
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var itemAdd: UIBarButtonItem!
+    
+    private let bag = DisposeBag()
+    private let images = Variable<[UIImage]>([])
+    private var imageCache = [Int]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sharedImages = images.asObservable().share()
+        
+        sharedImages
+            .throttle(0.5, scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] photos in
+                guard let preview = self?.imagePreview else { return }
+                preview.image = UIImage.collage(images: photos,
+                                                size: preview.frame.size)
+            })
+            .disposed(by: bag)
+        
+        sharedImages
+            .subscribe(onNext: { [weak self] photos in
+                self?.updateUI(photos: photos)
+            })
+            .disposed(by: bag)
+        
+    }
+    
+    private func updateUI(photos: [UIImage]) {
+        buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+        buttonClear.isEnabled = photos.count > 0
+        itemAdd.isEnabled = photos.count < 6
+        title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("resources: \(RxSwift.Resources.total)")
+    }
+    
+    @IBAction func actionClear() {
+        images.value = []
+        imageCache = []
+    }
+    
+    @IBAction func actionSave() {
+        guard let image = imagePreview.image else { return }
+        
+        PhotoWriter.save(image)
+            .subscribe(onError: { [weak self] error in
+                self?.showMessage("Error", description: error.localizedDescription)
+                }, onCompleted: { [weak self] in
+                    self?.showMessage("Saved")
+                    self?.actionClear()
+            })
+            .disposed(by: bag)
+    }
+    
+    @IBAction func actionAdd() {
+        //images.value.append(UIImage(named: "IMG_1907.jpg")!)
+        let photosViewController = storyboard!.instantiateViewController(
+            withIdentifier: "PhotosViewController") as! PhotosViewController
+        
+        let newPhotos = photosViewController.selectedPhotos.share()
+        
+        newPhotos
+            .takeWhile { [weak self] image in
+                return (self?.images.value.count ?? 0) < 6
+            }
+            .filter { newImage in
+                return newImage.size.width > newImage.size.height
+            }
+            .filter { [weak self] newImage in
+                let len = UIImagePNGRepresentation(newImage)?.count ?? 0
+                guard self?.imageCache.contains(len) == false else {
+                    return false
+                }
+                self?.imageCache.append(len)
+                return true
+            }
+            .subscribe(onNext: { [weak self] newImage in
+                guard let images = self?.images else { return }
+                images.value.append(newImage)
+                }, onDisposed: {
+                    print("completed photo selection")
+            })
+            .disposed(by: photosViewController.bag)
+        
+        
+        newPhotos
+            .ignoreElements()
+            .subscribe(onCompleted: { [weak self] in
+                self?.updateNavigationIcon()
+            })
+            .disposed(by: photosViewController.bag)
+        
+        navigationController!.pushViewController(photosViewController, animated: true)
+    }
+    
+    func showMessage(_ title: String, description: String? = nil) {
+        alert(title: title, text: description)
+            .subscribe()
+            .disposed(by: bag)
+    }
+    
+    private func updateNavigationIcon() {
+        let icon = imagePreview.image?
+            .scaled(CGSize(width: 22, height: 22))
+            .withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: icon,
+                                                           style: .done, target: nil, action: nil)
+    }
 }
